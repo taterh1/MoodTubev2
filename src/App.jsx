a5b3e243e47137cfcede20e68227763111617fc4
Taylor <taterh1@gmail.com>
2024-11-06 21:12:25 -0500

    initial start

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+    const [happy, setHappy] = useState([])
+    const [sad, setSad] = useState([])
+    const [excited, setExcited] = useState([])
+
+    useEffect(
+        () => {
+            axios.get('https://www.googleapis.com/youtube/v3/search?part=snippet&q=happy&key=AIzaSyCa1WdmO_Lf9tdZIGhxVS1dabUKLVU-iy8')
+                .then(res => {
+                    setHappy(res.data.items)
+                })
+                .catch(err => {
+                    console.error(err)
+                })
+        }, []
+    )
+
+  return (
+      <>
+      <h1>Vite + React</h1>
+          <div className="card">
+              {
+                  happy.forEach(video => {
+                      <p>{video}</p>
+                  })
+              }
+      </div>
+    </>
+  )
+}
+
+export default App
